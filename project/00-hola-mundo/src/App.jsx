b5c38f554a1c1f5436139fae47f3bcc73a23b1e9
Pablo <pablomonardo@gmail.com>
2024-07-29 16:30:09 -0300

    Página de práctica

---
+++ project/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://www.w3schools.com/w3images/avatar5.png" alt="Avatar de una persona" />
+                <div>
+                    <strong>Pablo Monardo</strong>
+                    <span>@monardop</span>
+                </div>
+            </header>
+            <side>
+                <button>Follow</button>
+            </side>
+        </article>
+        
+    )
+}
